8bac2883bbc6612d3ccebd632381454cbc2f2118
Dao Zheng <daoyzheng@gmail.com>
2019-04-19 19:44:10 -0600

    Setup overall calculator layout

--- app/src/main/java/com/example/simpleapp/MainActivity.java
+++ app/src/main/java/com/example/simpleapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
@@ -12,97 +13,75 @@ import java.math.RoundingMode;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Numbers
+    private Button btnZero;
+    private Button btnOne;
+    private Button btnTwo;
+    private Button btnThree;
+    private Button btnFour;
+    private Button btnFive;
+    private Button btnSix;
+    private Button btnSeven;
+    private Button btnEight;
+    private Button btnNine;
+
+    // Operators
+    private ImageButton btnAdd;
+    private ImageButton btnSub;
+    private ImageButton btnMultiply;
+    private ImageButton btnDivide;
+    private ImageButton btnEqual;
+
+    // TextViews
+    private TextView resultDisplay;
+    private TextView operationDisplay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button addBtn = findViewById(R.id.AddBtn);
-        Button subtractBtn = findViewById(R.id.SubtractBtn);
-        Button multiplyBtn = findViewById(R.id.MultiplyBtn);
-        Button divideBtn = findViewById(R.id.DivideBtn);
+        // Setup View
+        setupView();
 
-
-        // Creates an on click event for the addBtn
-        addBtn.setOnClickListener(new View.OnClickListener() {
+        // Click Events for each number and display in TextView
+        btnZero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText firstNumEditText = findViewById(R.id.FirstNumEditText);
-                EditText secondNumEditText = findViewById(R.id.SecondNumEditText);
-                TextView resultTextView = findViewById(R.id.ResultTextView);
-                TextView operationTextView = findViewById(R.id.operationTextView);
 
-                // Get value from Edit Text
-                BigDecimal num1 = new BigDecimal(firstNumEditText.getText().toString());
-                BigDecimal num2 = new BigDecimal(secondNumEditText.getText().toString());
-
-                BigDecimal result = num1.add(num2);
-
-                operationTextView.setText("Add");
-                resultTextView.setText(String.valueOf(result));
             }
         });
 
-        // Create an onclick event for the subtractBtn
-        subtractBtn.setOnClickListener(new View.OnClickListener() {
+        btnOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText firstNumEditText = findViewById(R.id.FirstNumEditText);
-                EditText secondNumEditText = findViewById(R.id.SecondNumEditText);
-                TextView resultTextView = findViewById(R.id.ResultTextView);
-                TextView operationTextView = findViewById(R.id.operationTextView);
-
-                BigDecimal num1 = new BigDecimal(firstNumEditText.getText().toString());
-                BigDecimal num2 = new BigDecimal(secondNumEditText.getText().toString());
-
-                BigDecimal result = num1.subtract(num2);
-
-                operationTextView.setText("Subtract");
-                resultTextView.setText(String.valueOf(result));
+                resultDisplay.setText("1");
             }
         });
+    }
 
+    private void setupView() {
+        // Numbers
+        btnZero = findViewById(R.id.btn0);
+        btnOne = findViewById(R.id.btn1);
+        btnTwo = findViewById(R.id.btn2);
+        btnThree = findViewById(R.id.btn3);
+        btnFour = findViewById(R.id.btn4);
+        btnFive = findViewById(R.id.btn5);
+        btnSix = findViewById(R.id.btn6);
+        btnSeven = findViewById(R.id.btn7);
+        btnEight = findViewById(R.id.btn8);
+        btnNine = findViewById(R.id.btn9);
 
-        // Create an onclick event for multiplication
-        multiplyBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText firstNumEditText = findViewById(R.id.FirstNumEditText);
-                EditText secondNumEditText = findViewById(R.id.SecondNumEditText);
-                TextView resultTextView = findViewById(R.id.ResultTextView);
-                TextView operationTextView = findViewById(R.id.operationTextView);
+        // Operators
+        btnAdd = findViewById(R.id.btnAdd);
+        btnSub = findViewById(R.id.btnSub);
+        btnMultiply = findViewById(R.id.btnMultiply);
+        btnDivide = findViewById(R.id.btnDivide);
+        btnEqual = findViewById(R.id.btnEqual);
 
-                // Get value from Edit Text
-                BigDecimal num1 = new BigDecimal(firstNumEditText.getText().toString());
-                BigDecimal num2 = new BigDecimal(secondNumEditText.getText().toString());
-
-                BigDecimal result = num1.multiply(num2);
-
-                operationTextView.setText("Multiply by");
-                resultTextView.setText(String.valueOf(result));
-            }
-        });
-
-        // Create an onclick event for divideBtn
-        divideBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText firstNumEditText = findViewById(R.id.FirstNumEditText);
-                EditText secondNumEditText = findViewById(R.id.SecondNumEditText);
-                TextView resultTextView = findViewById(R.id.ResultTextView);
-                TextView operationTextView = findViewById(R.id.operationTextView);
-
-                // Get value from Edit Text
-                double num1 = Double.parseDouble(firstNumEditText.getText().toString());
-                double num2 = Double.parseDouble(secondNumEditText.getText().toString());
-
-                double result = num1 / num2;
-
-                operationTextView.setText("Divide by");
-                resultTextView.setText(String.valueOf(result));
-            }
-        });
-
-
+        // TextViews
+        resultDisplay = findViewById(R.id.text_display);
+        operationDisplay = findViewById(R.id.operation_display);
     }
 }
